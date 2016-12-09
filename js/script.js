9ff200d34e9ed2ca3fcd3031cf2b7b1c971eb5a1
Boaz Blake <boazblake@gmail.com>
2016-12-08 21:21:46 -0600

    WIP touchscreen

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
 const keys = document.querySelectorAll('.key')
+const audioArray = document.querySelectorAll('audio')
 
 function addAction(x) {
   const audio = document.querySelector(`audio[data-key="${x.keyCode}"]`) 
   const key = document.querySelector(`.key[data-key="${x.keyCode}"]`) 
-  if (audio) {audio.play(audio.currentTime = 0) && key.classList.add('playing')}
+  if (audio) {
+    audio.play(audio.currentTime = 0) && key.classList.add('playing')
+  }
 }
 
 function touchStart(x) {
-  const audio = document.querySelector(`audio[data-key="${x.keyCode}"]`) 
-  const key = document.querySelector(`.key[data-key="${x.keyCode}"]`) 
-  if (audio) {audio.play(audio.currentTime = 0) && key.classList.add('playing')}
+  var target = x.target.innerHTML
+  // console.log( audioArray.map(x => console.log(x.innerHTML)) )
+  audioArray.forEach(
+    y => (y.innerHTML === target)?  y.play() :()=>{}
+    )
 }
 
 function removeAction(e){
   if (e.propertyName === 'transform') {this.classList.remove('playing')}
 }
 
-window.addEventListener('keydown', addAction)
 
 keys.forEach( x => x.addEventListener('transitionend', removeAction))
 
-window.addEventListener("touchstart", touchStart, false);
+window.addEventListener('keydown', addAction)
+window.addEventListener("touchstart", touchStart, false)
