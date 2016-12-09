1922c4222d022b0322ce3f9759c4361c6860b6ec
Boaz Blake <boazblake@gmail.com>
2016-12-08 20:45:54 -0600

    WIP touchscreen

--- js/script.js
+++ js/script.js
@@ -6,6 +6,12 @@ function addAction(x) {
   if (audio) {audio.play(audio.currentTime = 0) && key.classList.add('playing')}
 }
 
+function touchStart(x) {
+  const audio = document.querySelector(`audio[data-key="${x.keyCode}"]`) 
+  const key = document.querySelector(`.key[data-key="${x.keyCode}"]`) 
+  if (audio) {audio.play(audio.currentTime = 0) && key.classList.add('playing')}
+}
+
 function removeAction(e){
   if (e.propertyName === 'transform') {this.classList.remove('playing')}
 }
@@ -13,3 +19,5 @@ function removeAction(e){
 window.addEventListener('keydown', addAction)
 
 keys.forEach( x => x.addEventListener('transitionend', removeAction))
+
+window.addEventListener("touchstart", touchStart, false);
