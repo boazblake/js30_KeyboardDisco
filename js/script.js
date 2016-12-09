bddd1329da35d1584bef64e167bfc277467a5bf6
Boaz Blake <boazblake@gmail.com>
2016-12-08 20:30:41 -0600

    WIP touchscreen

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
 const keys = document.querySelectorAll('.key')
 
-function addAction(x) {
+const addEventListeners = (function(){
+ if (document.addEventListener){
+    return function listener(el, evt, handler) {
+      el.addEventListener(evt, handler)
+    }
+  }
+  else {
+    return function(el, evt, handler){
+      el.addEventListener('on'+event, handler )
+    }
+  }
+}())
+
+
+function keypress(x) {
+  const audio = document.querySelector(`audio[data-key="${x.keyCode}"]`) 
+  const key = document.querySelector(`.key[data-key="${x.keyCode}"]`) 
+  if (audio) {audio.play(audio.currentTime = 0) && key.classList.add('playing')}
+}
+
+function mousepress(x) {
+  console.log(x.target)
+  const audio = document.querySelector(`audio[data-key="${x.keyCode}"]`) 
+  const key = document.querySelector(`.key[data-key="${x.keyCode}"]`) 
+  if (audio) {audio.play(audio.currentTime = 0) && key.classList.add('playing')}
+}
+
+function touchpress(x) {
+  console.log(x.target)
   const audio = document.querySelector(`audio[data-key="${x.keyCode}"]`) 
   const key = document.querySelector(`.key[data-key="${x.keyCode}"]`) 
   if (audio) {audio.play(audio.currentTime = 0) && key.classList.add('playing')}
@@ -10,6 +38,9 @@ function removeAction(e){
   if (e.propertyName === 'transform') {this.classList.remove('playing')}
 }
 
-window.addEventListener('keydown', addAction)
+addEventListeners(window, 'keydown', keypress)
+addEventListeners(window, 'click', mousepress)
+addEventListeners(window, 'touch', touchpress)
+// window.addEventListener('keydown', keypress)
 
 keys.forEach( x => x.addEventListener('transitionend', removeAction))
